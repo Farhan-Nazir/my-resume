c9633d7798f8ba622cb0c38eabd4ca3d6fb49168
Farhn-Nazir <nazirf3440@gmail.com>
2018-12-17 23:57:38 +0100

    18 - working on JSON & fetch data in proper way.

--- src/screens/Education/Education.js
+++ src/screens/Education/Education.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import "./Education.css";
+import Data from "../../static/data/data.json";
 
 class Education extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      educations: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ educations: [...Data.Education] });
+  }
+  
+
   render() {
-    return <div>My Education </div>;
+    return (
+      <div>
+        My Education
+        {this.state.educations.map(e => {
+          return (
+          <li key={e.id}>{e.title} | {e.Description}</li>);
+        })}
+      </div>
+    );
   }
 }
 
