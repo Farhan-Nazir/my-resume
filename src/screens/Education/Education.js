f35e3c0cc9f43002e6bcdde9008335554cdefb21
Farhn-Nazir <nazirf3440@gmail.com>
2018-12-18 21:55:40 +0100

    19 - modified with css, data.json got some id's

--- src/screens/Education/Education.js
+++ src/screens/Education/Education.js
@@ -1,68 +1,71 @@
 import React, { Component } from "react";
 import "./Education.css";
 import Data from "../../static/data/data.json";
-
 class Education extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      educations: []
+      educations: [],
+      otherQualifications: []
     };
   }
 
   componentWillMount() {
     this.setState({ educations: [...Data.Education] });
+    this.setState({ otherQualifications: [...Data.OtherQualifications] });
   }
 
   render() {
     return (
-      <div className="eduMain">
-        <div className="edu eduLeft">
-          <h2> Education </h2>
-          {this.state.educations.map(e => {
-            return (
-              <ul style={{ listStyle: "none" }}>
-                <li key={e.id}>
-                  <div>
-                    University: {e.school}
-                    <br />
-                    Degree: {e.title}
-                    <br />
-                    Description: {e.description}
-                    <br />
-                    Start Date: {e.startDate}
-                    <br />
-                    End Date: {e.endDate}
-                    <br />
-                  </div>
-                </li>
-              </ul>
-            );
-          })}
-        </div>
-
-        <div className="edu eduRight">
-          <h2> Other Qualifications </h2>
-          {this.state.educations.map(e => {
-            return (
-              <ul className="" style={{ listStyle: "none" }}>
-                <li key={e.id}>
-                  <div>
-                    University: {e.school}
-                    <br />
-                    Degree: {e.title}
-                    <br />
-                    Description: {e.description}
-                    <br />
-                    Start Date: {e.startDate}
-                    <br />
-                    End Date: {e.endDate}
-                    <br />
-                  </div>
-                </li>
-              </ul>
-            );
-          })}
+      <div>
+        <div className="eduMain">
+          <div className="edu eduLeft">
+            <h2> Education </h2>
+            {this.state.educations.map(e => {
+              return (
+                <ul style={{ listStyle: "none" }}>
+                  <li key={e.id}>
+                    <div>
+                      University: {e.school}
+                      <br />
+                      Degree: {e.title}
+                      <br />
+                      Description: {e.description}
+                      <br />
+                      Start Date: {e.startDate}
+                      <br />
+                      End Date: {e.endDate}
+                      <br />
+                    </div>
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
+          <div className="sexy_line" />
+          <div className="edu eduRight">
+            <h2> Other Qualifications </h2>
+            {this.state.otherQualifications.map(e => {
+              return (
+                <ul className="" style={{ listStyle: "none" }}>
+                  <li key={e.id}>
+                    <div>
+                      University: {e.school}
+                      <br />
+                      Degree: {e.title}
+                      <br />
+                      Description: {e.description}
+                      <br />
+                      Start Date: {e.startDate}
+                      <br />
+                      End Date: {e.endDate}
+                      <br />
+                    </div>
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
