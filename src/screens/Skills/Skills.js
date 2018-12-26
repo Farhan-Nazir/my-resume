b55d0a6ef5ea26309a13a1c4db7e9a4b028f4c0d
Seif Tabaja <saif_tabaja@hotmail.com>
2018-12-26 14:14:19 +0100

    skills screen is done with basic styling

--- src/screens/Skills/Skills.js
+++ src/screens/Skills/Skills.js
@@ -1,8 +1,65 @@
 import React, { Component } from "react";
+import Grow from "@material-ui/core/Grow";
+import "./Skills.css";
+import Data from "../../static/data/data.json";
+import { Paper, Typography } from "@material-ui/core";
 
 class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transition: true,
+      skills: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ skills: [...Data.Skills] });
+  }
   render() {
-    return <div>My Skills</div>;
+    const { transition } = this.state;
+    return (
+      <div>
+        <Grow
+          in={transition}
+          style={{ transformOrigin: "0 0 0" }}
+          {...(transition ? { timeout: 800 } : {})}
+        >
+          <div className="skillsDiv">
+            <Paper>
+              <Typography
+                align={"center"}
+                gutterBottom
+                variant={"display2"}
+                component={"h5"}
+              >
+                Skills
+              </Typography>
+              {this.state.skills.map((e, i) => {
+                return (
+                  <Paper key={i} elevation={20}>
+                    <Typography
+                      align={"center"}
+                      variant={"h6"}
+                      component={"h5"}
+                    >
+                      {e.skillName}
+                    </Typography>
+                    <Typography
+                      align={"center"}
+                      variant={"h6"}
+                      component={"h5"}
+                    >
+                      {e.skillLevel}
+                    </Typography>
+                  </Paper>
+                );
+              })}
+            </Paper>
+          </div>
+        </Grow>
+      </div>
+    );
   }
 }
 
