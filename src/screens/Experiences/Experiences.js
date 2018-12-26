5d47038f6d586071199e64d706b8215f5606d5b0
Seif Tabaja <saif_tabaja@hotmail.com>
2018-12-26 16:54:18 +0100

    experiences screen is done

--- src/screens/Experiences/Experiences.js
+++ src/screens/Experiences/Experiences.js
@@ -1,8 +1,72 @@
 import React, { Component } from "react";
+import Grow from "@material-ui/core/Grow";
+import "./Experience.css";
+import Data from "../../static/data/data.json";
+import { Paper, Typography } from "@material-ui/core";
 
 class Experiences extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transition: true,
+      experiences: []
+    };
+  }
+  componentWillMount() {
+    this.setState({ experiences: [...Data.Experiences] });
+  }
+
   render() {
-    return <div>work Experiences</div>;
+    const { transition } = this.state;
+    return (
+      <div>
+        <Grow
+          in={transition}
+          style={{ transformOrigin: "0 0 0" }}
+          {...(transition ? { timeout: 800 } : {})}
+        >
+          <div className="experiencesDiv">
+            <Paper style={{ padding: 10 }}>
+              <Typography
+                align={"center"}
+                gutterBottom
+                variant={"display2"}
+                component={"h5"}
+              >
+                Experience
+              </Typography>
+              {this.state.experiences.map((e, i) => {
+                return (
+                  <Paper key={i} elevation={20} style={{ padding: 20 }}>
+                    <Typography
+                      align={"center"}
+                      variant={"h6"}
+                      component={"h5"}
+                    >
+                      {e.companyName}
+                    </Typography>
+                    <Typography
+                      align={"center"}
+                      variant={"h6"}
+                      component={"h5"}
+                    >
+                      {e.description}
+                    </Typography>
+                    <Typography
+                      align={"center"}
+                      variant={"h6"}
+                      component={"h5"}
+                    >
+                      {e.startDate} -- {e.endDate}
+                    </Typography>
+                  </Paper>
+                );
+              })}
+            </Paper>
+          </div>
+        </Grow>
+      </div>
+    );
   }
 }
 
